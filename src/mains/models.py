8ef7f76d91d64b94f8e5fad02e2471f17d63b480
pramod0021a <45119851+pramod0021a@users.noreply.github.com>
2023-06-26 21:36:32 +0530

    Update models.py

--- src/mains/models.py
+++ src/mains/models.py
@@ -8,5 +8,8 @@ from django.db import models
 #    updated = models.DateTimeField(auto_now=True)
 #    created = models.DateTimeField(auto_now_add=True)
 
-#    def __str__(self):
-#       return str(self.name)
+    # def __str__(self):
+    #   return str(self.name)
+
+    # class Meta:
+    #   verbose_name_plural = "Customers"
